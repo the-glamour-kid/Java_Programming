d4bdb24ed9c45a26d62f2183af36361edc689f0d
the-glamour-kid <balumancha70365@gmail.com>
2022-10-16 10:25:49 +0530

    infinte for loop

---
+++ ApplicationProgramming/ForLoopTricks/InfiniteForLoop.java
@@ -0,0 +1,10 @@
+class InfiniteForLoop{
+    public static void main(String args[]){
+        int num = 0;
+        //this is the infinite for loop
+        for(;;){
+            num = num + 1;
+            System.out.println(num);
+        }
+    }
+}
